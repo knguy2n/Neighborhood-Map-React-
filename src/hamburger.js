582ad3ea35bd57c8b8d833726b6b312a4645e64f
Kien Nguyen <kdot@Kiens-MacBook-Pro.local>
2018-10-01 22:05:03 -0700

    Add styles

--- src/hamburger.js
+++ src/hamburger.js
@@ -9,12 +9,19 @@ class Hamburger extends React.Component {
 
   render () {
     return (
-      <Menu>
-        <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/about">About</a>
-        <a id="contact" className="menu-item" href="/contact">Contact</a>
-        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-      </Menu>
+    	<div height={'100px'} >
+    		<Menu width={'20%'}>
+       			 <a id="home" className="menu-item" href="/">Home</a>
+       			 <a id="about" className="menu-item" href="/about">About</a>
+        		<a id="contact" className="menu-item" href="/contact">Contact</a>
+        		<a href="" className="menu-item">Test</a>
+        		<a onClick={ this.showSettings } className="menu-item" href="">Settings</a>
+      		</Menu>
+    	</div>
+
+    	
+      
+
     );
   }
 }
---
+++ src/title.js
@@ -0,0 +1,24 @@
+// title
+import React, { Component } from 'react';
+import Hamburger from './hamburger'
+
+
+
+class Title extends React.Component {
+
+	render(){
+		return(
+			<div className="title">
+				<h1>Kien's Neighborhood Map</h1>
+				<Hamburger/>
+
+			</div>
+
+
+			)
+	}
+
+
+}
+
+export default Title;
